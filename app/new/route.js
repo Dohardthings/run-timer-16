23886e13f37e231e59607ec498ae365244968e54
Daniel McBryde <mcbryded@gmail.com>
2016-04-09 13:09:55 -0500

    repair gap in knowledge and restructure html

--- app/new/route.js
+++ app/new/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    const url = `http://tiny-tn.herokuapp.com/collections/runs-dm`;
+    return fetch(url).then(res => res.json()).then((data) => {
+
+    });
+  },
+
 });
