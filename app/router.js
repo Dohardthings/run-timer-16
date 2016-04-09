18ceb4b3a61888ba4272fe682d51627495019b86
Daniel McBryde <mcbryded@gmail.com>
2016-04-09 16:58:42 -0500

    route index to details

--- app/router.js
+++ app/router.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
   this.route('new');
-  this.route('detail');
+  this.route('detail',{ path: `/:_id` });
+  this.route('index');
 });
 
 export default Router;
