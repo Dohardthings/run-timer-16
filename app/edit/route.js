463cf8ed4bf048b6e8869e4545f8873c0ae1d1be
Daniel McBryde <mcbryded@gmail.com>
2016-04-10 13:38:33 -0500

    build edit page

--- app/edit/route.js
+++ app/edit/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(url) {
+    return fetch(`https://tiny-tn.herokuapp.com/collections/runs-dm/${url._id}`)
+      .then(resp => resp.json());
+  },
+
 });
